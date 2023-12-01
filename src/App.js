60e800aa6bdbd28474d70d78e1b3820939801820
Momoka1113 <21l1286@andrew.ac.jp>
2023-12-01 20:50:28 +0900

    2

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This Site is Profile Site By Momotan
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>自己紹介</h1>
+      <p>momoka</p>
+      <p>好きなキャラクター・・・マイメロ</p>
+      <p>バイト・・・ラーメン屋</p>
+       <p>今ハマっているもの・・・仮面ライダーＷ</p>
     </div>
   );
 }
